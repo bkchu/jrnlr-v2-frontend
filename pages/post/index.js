44b951d510034290280a6d977574cf40e37eacbd
Brandon Chung <brandon.k.chung@gmail.com>
2019-09-11 14:19:09 -0500

    remove default color, add title

--- pages/post/index.js
+++ pages/post/index.js
@@ -9,6 +9,7 @@ import { Loader } from '../../components/Loader';
 import { ME_QUERY } from '../../components/Query/Me';
 import { POST_QUERY } from '../../components/Query/Post';
 import { Image, Text } from '../../design';
+import Head from 'next/head';
 
 function Post() {
   const router = useRouter();
@@ -29,56 +30,43 @@ function Post() {
   const isMe = data && data.me && data.me.id === post.author.id;
 
   return (
-    <Box key={post.id} maxWidth="75rem" mt={[2, 3]} mb="7" mx="auto">
-      {post.imgurl && <Image src={post.imgurl} alt="" mb="1" />}
-      <Text
-        color="grays.1"
-        fontSize={[5, 6]}
-        fontWeight="6"
-        lineHeight="title"
-        mb="3"
-      >
-        {post.title}
-      </Text>
-      <Text color="grays.1" fontSize={[3, 4]} fontWeight="2" mb="2">
-        {post.subtitle}
-      </Text>
-      <Text
-        color="grays.1"
-        fontSize={[2, 3]}
-        letterSpacing="tracked"
-        fontWeight="2"
-        mb="3"
-      >
-        @{post.author.username} -{' '}
-        <span style={{ fontStyle: 'italic' }}>
-          {moment(post.createdAt).fromNow()}
-        </span>
-      </Text>
-      {isMe && (
-        <Box>
-          <Link
-            href={{
-              pathname: `/post/${slug(post.title).toLowerCase()}/edit`,
-              query: { id: post.id }
-            }}
-          >
-            <Button small secondary>
-              Edit
-            </Button>
-          </Link>
-        </Box>
-      )}
-      <Text
-        fontFamily="sans"
-        color="grays.1"
-        fontSize={[2, 3]}
-        fontWeight="4"
-        my={4}
-      >
-        {post.content}
-      </Text>
-    </Box>
+    <>
+      <Head>
+        <title>
+          {post.title} | {post.author.name}
+        </title>
+      </Head>
+      <Box key={post.id} maxWidth="75rem" mt={[2, 3]} mb="7" mx="auto">
+        {post.imgurl && <Image src={post.imgurl} alt="" mb="1" />}
+        <Text fontSize={[5, 6]} fontWeight="6" lineHeight="title" mb="3">
+          {post.title}
+        </Text>
+        <Text fontSize={[3, 4]} fontWeight="2" mb="2">
+          {post.subtitle}
+        </Text>
+        <Text fontSize={[2, 3]} letterSpacing="tracked" fontWeight="2" mb="3">
+          @{post.author.username} -{' '}
+          <span style={{ fontStyle: 'italic' }}>
+            {moment(post.createdAt).fromNow()}
+          </span>
+        </Text>
+        {isMe && (
+          <Box>
+            <Link
+              href={{
+                pathname: `/post/${slug(post.title).toLowerCase()}/edit`,
+                query: { id: post.id }
+              }}
+            >
+              <Button small>Edit</Button>
+            </Link>
+          </Box>
+        )}
+        <Text fontFamily="sans" fontSize={[2, 3]} fontWeight="4" my={4}>
+          {post.content}
+        </Text>
+      </Box>
+    </>
   );
 }
 
